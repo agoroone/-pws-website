4e6d65a759e429a3074de5a7dd6f4caef8ce88c3
agoroone <omotagoro@gmail.com>
2022-07-22 18:38:57 +0100

    interactive website

---
+++ myDom.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector(".btn");
+
+
+btn.addEventListener("mouseleave", (e) => {
+  e.preventDefault();
+  console.log(e.target.className);
+
+  document.querySelector("header").style.background = "lightblue"
+  
+    document.querySelector("footer").style.background = "black";
+    document.querySelector("body").style.background = "white";
+    document.querySelector("body").style.fontFamily =   "Verdana";
+//   document.querySelector("body").classList.add("bg-dark");
+//   ulElements.lastElementChild.innerHTML = "<h1>Design Thinking</h1>";
+});
+
+
+btn.addEventListener("mouseover", (e) => {
+    e.preventDefault();
+    console.log(e.target.className);
+  
+      document.querySelector("header").style.background = "white";
+      document.querySelector("footer").style.background = "black";
+      document.querySelector("body").style.background = "red";
+      document.querySelector("body").style.fontFamily =   "Georgia";
+      document.querySelector("a").style.color=   "black";
+  
+  //   document.querySelector("body").classList.add("bg-dark");
+  //   ulElements.lastElementChild.innerHTML = "<h1>Design Thinking</h1>";
+  });
